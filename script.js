fa32b92bb8bd763f3f32daa1abfc0fdbdd620292
K-D-Piper <kirsten.piper@mscoding.org>
2024-09-28 17:42:12 -0500

    Cleaned up code. Finished Total Bees add
    Spaced out code, so its easier to read. Updated some names so theyd bother me less. Added TotalBees count to javascript. Finished. Started working on Avaliable bees

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-let test = "cry bish"
-console.log(test)
+//Total Bee Count START
+let count = 0;
+let queenButton = document.getElementById("clickQueen")
+let totalBees = document.getElementById("totalDisplay")
+
+
+queenButton.addEventListener('click', () => 
+    {
+        count+= 1;
+        totalBees.innerHTML = count
+    })
+console.log(queenButton)
+//Total Bee Count END
+
+let beesAvaliable = document.getElementById("avaliableDisplay")
+
